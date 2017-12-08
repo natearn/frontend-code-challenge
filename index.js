b58b3a8d96fb063c84fb55dcdea18330ba30c3c5
Nathan Arnold <natearn@natearn.info>
2017-12-08 13:48:53 +0100

    Add all the required data fields
    I've created dummy data for all the required information for a
    property ad, as well as a very naive implementation of the component.
    
    Now, I need to decide how to do the styling.

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ import { render } from "react-dom"
 import { App } from "components/App.jsx"
 
 let dummyData = {
-	ads: [
-		{
-			id: 1,
-			photo: "photo.jpg"
-		},
-		{
-			id: 2,
-			photo: "photo.jpg"
-		},
-		{
-			id: 3,
-			photo: "photo.jpg"
-		},
-	]
+	ads: [1,2,3,4,5].map((_,idx) =>
+		({
+			id: idx,
+			photo: "photo.jpg",
+			action: "Mieten",
+			desc: "This is a property description. For the purpose of styling, " +
+				  "I'd like to make this reasonably long. This is not mean to be " +
+				  " informative!",
+			addr: "74635 Sinn / Fleisbach",
+			price: 4398.00,
+			rooms: 2,
+			size: 96,
+		})
+	)
 }
 
 render(
--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,8 +1,28 @@
 import React from "react" // required to define components
 
-export const Property = ({ photo }) => (
+export const Property = ({
+			photo,
+			action,
+			desc,
+			addr,
+			price,
+			rooms,
+			size,
+}) => (
 	<div>
-		<img src={photo} />
+		<div>
+			<img src={photo} />
+			<button>{action}</button>
+		</div>
+		<div>
+			<p>{desc}</p>
+			<div>{addr}</div>
+			<div>
+				<span>{price}</span>
+				<span>{rooms}</span>
+				<span>{size}</span>
+			</div>
+		</div>
 	</div>
 )
 
