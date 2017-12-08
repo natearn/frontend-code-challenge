8e7794906fcbe91aa2e5e9c5e1d9ea96a987f899
Nathan Arnold <natearn@natearn.info>
2017-12-08 12:50:05 +0100

    Allow start.sh to build the docker images
    If the image doesn't exist, the start script will build it from
    the current directory.

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ IMAGE_NAME="mcmakler"
 CONTAINER="challenge"
 COMMAND="sh"
 
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" == "" ]];
+then docker build -t $IMAGE_NAME .
+fi
+
 if docker inspect -f '{{.State.Running}}' "$CONTAINER" &> /dev/null
 then docker exec -it "$CONTAINER" "$COMMAND"
 else docker run -it --rm --name "$CONTAINER" \
