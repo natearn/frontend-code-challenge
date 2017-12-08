edbae968d539f5c2191b6949c6c155890642738d
Nathan Arnold <natearn@natearn.info>
2017-12-08 15:59:36 +0100

    List property ads horizontally
    I'm using flexbox for better layout control. I'm using px units so
    that the interface doesn't scale weirdly on various font sizes or
    display sizes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
 import React from "react" // required to define components
+import styles from "components/App.css"
 import { Property } from "components/Property"
 
 export const App = ({ ads }) => (
-	<ul>
-		{ads.map(ad => (
+	<ul className={ styles.app || "app" }>
+		{ads.map( ad => (
 			<li key={ad.id}>
 				<Property {...ad} />
 			</li>
 		))}
 	</ul>
 )
-
