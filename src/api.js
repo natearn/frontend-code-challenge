aa9b6594068c81dc0c1bd204696e4004ba7901d3
Nathan Arnold <natearn@natearn.info>
2017-12-12 14:07:24 +0100

    Touch up the api methods

--- src/api.js
+++ src/api.js
@@ -1,16 +1,17 @@
 // This module is responsible for implementing the API client as well as
 // simplifying the responses to make them easier to work with
 
-// get the ads as a Promise, easy
+// get the ads as a Promise
 export const fetchJSON = () => {
 
 	let url = new URL("https://api.mcmakler.de/v1/advertisements")
 	let headers = new Headers()
+	headers.append("Accept","application/json")
 
 	const context = {
 		method: 'GET',
 		headers: headers,
-		mode: 'cors',
+		mode: 'no-cors',
 		cache: 'default'
 	}
 
@@ -18,14 +19,16 @@ export const fetchJSON = () => {
 		.then(res => {
 			if (res.ok) {
 				return res.json()
+			} else {
+				throw new Error("API response was not OK")
 			}
-			throw new Error("API response was not OK")
 		})
 }
 
 // transform a successful response json into something that is easier to
 // consume by the application (such as the App props object)
 // TODO: make this less painful to read
+// TODO: get the reselect library for this!
 export const simplify = json => ({
 	ads: json.data.map(ad => ({
 
